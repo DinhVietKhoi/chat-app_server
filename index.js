a6ae8c699daac21e98a4f1d3b03d94f4cdbb989e
Khoi <dinhvietkhoi99@gmail.com>
2023-12-11 19:58:31 +0700

    second

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import { Server } from 'socket.io';
+import messageRoutes from '../server/routes/messagesRoute.js'
 import userRoutes  from '../server/routes/userRoute.js'
-import messageRoutes  from '../server/routes/messagesRoute.js'
+
 const app = express();
 
 dotenv.config();
